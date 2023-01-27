51faecc6f563cac96c4f203a7626f3624fc75a6d
NAGARAJAN E <98300427+NAGARAJANELAN@users.noreply.github.com>
2023-01-27 23:53:27 +0530

    adding api

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
     const fetchPost=async ()=>{
       setLoading(true);
       try {
-        const response= await fetch("http://localhost:8090/api/v1/post",
+        const response= await fetch("https://nax-ai-dall-e.onrender.com/api/v1/post",
         {
           method:'GET',
           headers:{
